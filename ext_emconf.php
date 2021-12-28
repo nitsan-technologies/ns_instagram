f8f7895d61d49690de3cdc32ac83ca4b4d5e0653
NITSAN Technologies <sanjay@nitsan.in>
2021-12-28 11:21:49 +0530

    [TASK] Addded Live Demo Page

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,10 +1,10 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => '[NITSAN] Instagram Plugin',
+    'title' => '[NITSAN] Instagram TYPO3 Plugin',
     'description' => 'Plugin to display gallery, feeds and phototile with clean, customizable and responsive feeds from multiple Instagram accounts. Demo: https://demo.t3terminal.com/t3t-extensions/ You can download PRO version for more-features & free-support at https://t3terminal.com/typo3-instagram-plugin-free/',
     'category' => 'plugin',
-    'author' => 'NITSAN Technologies Pvt Ltd',
+    'author' => 'Team NITSAN',
     'author_email' => 'sanjay@nitsan.in',
     'author_company' => 'NITSAN Technologies Pvt Ltd',
     'state' => 'stable',
